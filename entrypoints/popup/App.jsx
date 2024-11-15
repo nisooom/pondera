4f9a1822d6eb51a00a430d3d0c0bf8ba272976c4
TejasBhovad <tejasbhovad@gmail.com>
2024-11-15 15:24:02 +0530

    feat: remove typescript

--- entrypoints/popup/App.jsx
+++ entrypoints/popup/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
